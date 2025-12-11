5e079022d2707f93a50fd2c1cd82151f1a32a31b
ruthesieboma42 <ruthesieboma@gmail.com>
2025-12-11 09:23:40 +0100

    fixed nativewind issue and added splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
 import { Stack } from "expo-router";
 import './global.css';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontsError] = useFonts({
+    // Load your custom fonts here
+    // 'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+   console.log("Fonts loaded:", fontsLoaded, "Error:", fontsError);
+    if (fontsLoaded || fontsError) {
+      // Hide the splash screen after fonts are loaded
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
+
+  return (
+    <Stack/>
+   
+  );
+
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native";
 import "./global.css";
  
-export default function App() {
+export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
@@ -10,4 +10,3 @@ export default function App() {
     </View>
   );
 }
-
