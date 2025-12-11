44cd037ffc3301c036670540029e8a5fe62d7b2f
ruthesieboma42 <ruthesieboma@gmail.com>
2025-12-11 17:38:37 +0100

    Updated Assets - included fonts, icons and created constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx",    "./App/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purple: {
+          100: '#DBCDF0',
+          200 : '9747FF'
+        },
+        green:{
+          100: '#C9E4DF'
+        },
+        white:{
+          100: '#FFFFFF'
+        },
+        black: {
+          100: '#000000'
+        },
+        orange: {
+          100:'#F8D9C4'
+        },
+        blue: {
+          100: '#D9E8FC'
+        },
+        pink: {
+          100: '#F2C6DF'
+        },
+        yellow:{
+          100: '#FAEDCB'
+        }
+      }
+    },
   },
   plugins: [],
 }
